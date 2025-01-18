62697f84bc654f4dd9cc958e950ef08c6e3977e2
ilhamarfian <ilham.arfian@btpn.com>
2025-01-18 12:08:48 +0700

    add testify to perform assertions

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,6 +1,8 @@
 package helper
 
 import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -13,4 +15,17 @@ func TestHelloWorld(t *testing.T) {
 		//t.Fatal() logs an error message and stops execution
 		t.Error("Expected 'Hello, John', got", result)
 	}
+}
+
+// TestHelloWorldAssert unit test using Testify to perform assertions
+func TestHelloWorldAssert(t *testing.T) {
+	result := HelloWorld("John")
+	assert.Equal(t, "Hello, John", result, "Expected 'Hello, John'")
+}
+
+// TestHelloWorldRequire unit test using Testify to perform assertions and stops execution if assertion fails
+func TestHelloWorldRequire(t *testing.T) {
+	result := HelloWorld("John")
+	//invoke t.FailNow() if assertion fails
+	require.Equal(t, "Hello, John", result, "Expected 'Hello, John'")
 }
